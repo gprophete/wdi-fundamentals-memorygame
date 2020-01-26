09f9b5f9a520c8d1c5b90d97c72d0b5245a7b548
gprophete <garlyprophete30@gmail.com>
2020-01-26 18:25:43 -0500

    created game logic

---
+++ memory-game/JS/main.js
@@ -0,0 +1,11 @@
+console.log("up and running!");
+
+let cardOne = "Queen of Diamonds";
+
+let cardTwo = "Queen of Hearts";
+
+let cardThree = "King of Diamonds";
+
+let cardFour = "King of Hearts";
+
+console.log("User flipped" + " " + cardOne + " and " + cardFour);
