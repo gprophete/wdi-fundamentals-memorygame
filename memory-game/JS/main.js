5494ce9565aa143184b52e5648bca841f0cfea75
gprophete <garlyprophete30@gmail.com>
2020-01-28 01:33:41 -0500

    created gamelogic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -5,29 +5,23 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
+cardsInPlay.push(cards[0, 2]);
 
-cardsInPlay.push('cardOne');
-
-console.log("User Flipped" + cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push('cardTwo');
-
-console.log("User Flipped" + cardTwo);
-
-cardsInPlay.length;
-
-if (cardsInPlay.length != 2) {
-	alert("You found a match");
-} 
-else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match");
-}
-else {
-	alert("Sorry, try again");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+};
 }
 
+
+function flipCard(cardID){
+	console.log("User flipped" + cards[cardID]);
+	
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
 
